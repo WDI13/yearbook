82ba4ec6baed7511746bf3aacdaa4b4b6e676ec1
David Haynes <dhaynesxx@gmail.com>
2016-05-03 17:21:42 +1000

    setout tables, stopped man running over edges, added link squares though not yet linked

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ personControls = {
     new: function(name) {
         person = {
                 name: name,
-                row: 26,
+                row: 28,
                 column: 9,
                 angle: 1
         };
@@ -56,16 +56,73 @@ personControls = {
         }
         this.place(person);
     },
+    testMove: function(row, column) {
+        if (row === 0) {
+            return true;
+        } else if (row === 29) {
+            return true;
+        } else if (column === 0) {
+            return true;
+        } else if (column === 18) {
+            return true;
+        } else if (row > 4 && row < 25 && column === 6) {
+            return true;
+        } else if (row > 4 && row < 25 && column === 12) {
+            return true;
+        } else if (column > 5 && column < 13 && row === 5) {
+            return true;
+        } else if (column > 5 && column < 13 && row === 25) {
+            return true;
+        }
+        return false;
+    },
 
     forward: function(person) {
         if (person.angle === 1) {
+            if (this.testMove(person.row -1, person.column)) {
+                return;
+            }
             person.row -= 1;
         } else if (person.angle === 2) {
+            if (this.testMove(person.row, person.column + 1)) {
+                return;
+            }
             person.column += 1;
         } else if (person.angle === 3) {
+            if (this.testMove(person.row +1, person.column)) {
+                return;
+            }
             person.row += 1;
         } else if (person.angle === 4) {
+            if (this.testMove(person.row, person.column - 1)) {
+                return;
+            }
             person.column -= 1;
+        }
+        this.place(person);
+    },
+
+    backward: function(person) {
+        if (person.angle === 1) {
+            if (this.testMove(person.row +1, person.column)) {
+                return;
+            }
+            person.row += 1;
+        } else if (person.angle === 2) {
+            if (this.testMove(person.row, person.column - 1)) {
+                return;
+            }
+            person.column -= 1;
+        } else if (person.angle === 3) {
+            if (this.testMove(person.row -1, person.column)) {
+                return;
+            }
+            person.row -= 1;
+        } else if (person.angle === 4) {
+            if (this.testMove(person.row, person.column + 1)) {
+                return;
+            }
+            person.column += 1;
         }
         this.place(person);
     },
@@ -99,11 +156,16 @@ var board = {
 
 };
 
+var linkCells = ['cell2501', 'cell2001', 'cell1401', 'cell0801', 'cell0401', 'cell0103', 'cell0107', 'cell0111', 'cell0115', 'cell0417', 'cell0817', 'cell1417', 'cell1917', 'cell2517', 'cell2205', 'cell1605', 'cell1005', 'cell0409', 'cell1013', 'cell1613', 'cell2213'];
+
 $(function() {
-    board.make(3,17,1,1,'#top-walk');
-    board.make(21,5,4,1,'#left-walk');
-    board.make(21,5,4,13,'#right-walk');
-    board.make(3,17,25,1,'#bottom-walk');
+    board.make(4,17,1,1,'#top-walk');
+    board.make(21,5,5,1,'#left-walk');
+    board.make(21,5,5,13,'#right-walk');
+    board.make(3,17,26,1,'#bottom-walk');
+    for (var i = 0; i < linkCells.length; i++) {
+        $('#' + linkCells[i]).addClass('linkCell');
+    }
     personControls.new('Wolf');
     $('body').keydown(function(e){
         if(e.keyCode == 37){
@@ -117,6 +179,10 @@ $(function() {
         if(e.keyCode == 38){
             e.preventDefault();
             personControls.forward(person);
+        }
+        if(e.keyCode == 40){
+            e.preventDefault();
+            personControls.backward(person);
         }
     });
 
